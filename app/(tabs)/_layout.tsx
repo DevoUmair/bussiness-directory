ee81581bec6c9aa8a6ff58e2c0b3db1d79c010d3
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2025-07-02 13:02:26 +0500

    chamges update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,23 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    <Tabs screenOptions={{
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    }} >
+        <Tabs.Screen name='home' 
+            options={{
+                tabBarLabel:'Home',
+                tabBarIcon: ({ color }) => (
+                    <svg width="24" height="24" fill={color}>
+                        <path d="M12 2L1 9h3v11h16V9h3L12 2zM5 20V9.83l7-5.33 7 5.33V20H5z"/>
+                    </svg>
+                ),
+            }}
+        />
+        <Tabs.Screen name='explore' />
+        <Tabs.Screen name='profile' />
     </Tabs>
-  );
+  )
 }
---
+++ app/(tabs)/explore.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const explore = () => {
+  return (
+    <View>
+      <Text>explorsasaae</Text>
+    </View>
+  )
+}
+
+export default explore
